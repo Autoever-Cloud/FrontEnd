b2328ad5d5695fa1f0f05dc6947a6dfe50b319bd
YangjiwooGN <jwoo3460@gmail.com>
2025-10-14 14:19:10 +0900

    feat)mainpage ment fix

--- src/views/MainPage/index.js
+++ src/views/MainPage/index.js
@@ -86,7 +86,7 @@ const FrontBox = styled.div`
     border-radius: 6px;
     padding: 38px 45px;
     color: #f0f4ff;
-    font-size: 18px;
+    font-size: 16px;
     font-weight: 600;
     line-height: 1.9;
     text-align: left; /* ✅ 왼쪽 정렬 */
@@ -171,12 +171,12 @@ function MainPage() {
                     <BackBox />
                     <FrontBox>
                         <p>
-                            SOLOg는 Log 데이터를 활용해 문제 해결(SOLVE)을 보조하는 <br />
+                            SOLOg는 Log 데이터를 활용해 <br />문제 해결(SOLVE)을 보조하는 <br />
                             통합 시각화, 알림 서비스입니다.
                         </p>
                         <p>
                             Kibana를 통한 로그 정보 모니터링, <br />
-                            Grafana를 통한 매트릭 모니터링 기능을 제공합니다.
+                            Grafana를 통한 매트릭 모니터링 <br /> 기능을 제공합니다.
                         </p>
                         <p>
                             사전 정의된 규칙에 따라 발생된 알림을 모아 확인하고 <br />
